554fd3dd4775a8d473d96b7312f752037ff05bec
Mehedi Hasan Siam <mehedisiam0022@gmail.com>
2021-09-03 19:43:37 +0600

    update

--- js/app.js
+++ js/app.js
@@ -9,16 +9,17 @@ const defaultWeather = () => {
 }
 defaultWeather();
 
-const defaultDisplay = (cityData) => {
+const defaultDisplay = (city) => {
     const weatherResult = document.getElementById('weather-result');
     weatherResult.innerHTML = `
-    <h1>${cityData.name}</h1>
-    <h5>${cityData.weather[0].main}</h5>
-    <h5>${cityData.main.temp}&deg;C</h5>
-    <p>Feels Like ${cityData.main.feels_like}&deg;C</p>
-    <p>Min Temp Today: ${cityData.main.temp_min}&deg;C</p>
-    <p>Max Temp Today: ${cityData.main.temp_max}&deg;C</p>
-    <p>Wind pressure: ${cityData.wind.speed} Km/h</p>
+    <h1 class="d-inline-block">${city.name}</h1>
+    <span> (${city.sys.country})</span>
+    <h5>${city.weather[0].main}</h5>
+    <h5>${city.main.temp}&deg;C</h5>
+    <p>Feels Like ${city.main.feels_like}&deg;C</p>
+    <p>Min Temp Today: ${city.main.temp_min}&deg;C</p>
+    <p>Max Temp Today: ${city.main.temp_max}&deg;C</p>
+    <p>Wind pressure: ${city.wind.speed} Km/h</p>
     `
 };
 
@@ -34,10 +35,15 @@ const loadWeather = () => {
 };
 
 const displayWeather = (cityData) => {
-    // console.log(cityData);
+    console.log(cityData);
+
+    const url = (`http://openweathermap.org/img/wn/${cityData.weather[0].icon}@2x.png`)
+    const wIcon = document.getElementById('w-icon');
+    wIcon.setAttribute('src', url);
     const weatherResult = document.getElementById('weather-result');
     weatherResult.innerHTML = `
-    <h1>${cityData.name}</h1>
+    <h1 class="d-inline-block">${cityData.name}</h1>
+    <span> (${cityData.sys.country})</span>
     <h5>${cityData.weather[0].main}</h5>
     <h5>${cityData.main.temp}&deg;C</h5>
     <p>Feels Like ${cityData.main.feels_like}&deg;C</p>
